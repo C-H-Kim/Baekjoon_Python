f9a1c92541428ac9856638aecd13fadb480be8c0
Chang hun <nick6253@naver.com>
2022-12-08 17:38:59 +0900

    Basic Skills / A+B - 8

---
+++ week0_Basic Skills/BJ_11022.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    T = int(input())
+
+    for test_case in range(1, T + 1):
+        A, B = map(int, input().split())
+
+        print(f"Case #{test_case}: {A} + {B} = {A + B}")
