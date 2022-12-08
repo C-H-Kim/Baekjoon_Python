4f12007f1c56d38731065862edb3a7c4d7b06345
Chang hun <nick6253@naver.com>
2022-12-08 16:52:46 +0900

    Basic Skills / 알람 시계

---
+++ week0_Basic Skills/BJ_2884.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    H, M = map(int, input().split())
+
+    M -= 45
+    if M < 0:
+        H -= 1
+        M += 60
+
+    if H < 0:
+        H += 24
+
+    print(f"{H} {M}")
