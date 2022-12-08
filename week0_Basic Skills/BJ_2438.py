1355252b75ec30051f82faad5d785d5e120533dd
Chang hun <nick6253@naver.com>
2022-12-08 17:46:31 +0900

    Basic Skills / 별 찍기 - 1

--- week0_Basic Skills/BJ_2438.py
+++ week0_Basic Skills/BJ_2438.py
@@ -2,6 +2,6 @@ if __name__ == '__main__':
     N = int(input())
 
     for i in range(1, N + 1):
-        for j in range(i):
+        for _ in range(i):
             print("*", end='')
         print()
