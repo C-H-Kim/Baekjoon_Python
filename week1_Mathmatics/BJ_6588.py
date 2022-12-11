91ce98e48a6e274a710f9465c5513d78f025010b
Chang hun <nick6253@naver.com>
2022-12-12 02:05:55 +0900

    Mathmatics / 골드바흐의 추측

---
+++ week1_Mathmatics/BJ_6588.py
@@ -0,0 +1,34 @@
+def is_prime():
+    global arr
+    arr[0] = arr[1] = False
+
+    for i in range(2, len(arr)):
+        if arr[i]:
+            j = 2
+            while i * j < len(arr):
+                arr[i * j] = False
+                j += 1
+
+    return
+
+
+arr = [True] * 1000001
+is_prime()
+
+while True:
+    n = int(input())
+
+    if n == 0:
+        break
+
+    a = 0
+    b = n
+    flag = False
+    while True:
+        if arr[a] and arr[b]:
+            print(f"{n} = {a} + {b}")
+            flag = True
+            break
+
+        a += 1
+        b -= 1
