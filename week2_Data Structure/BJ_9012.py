a75fa1c3ecc1e4eb02af89763ae7a0361c7bf751
Chang hun <nick6253@naver.com>
2022-12-15 03:34:08 +0900

    Data Structure / 괄호

---
+++ week2_Data Structure/BJ_9012.py
@@ -0,0 +1,47 @@
+import sys
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, data):
+        self.items.append(data)
+
+    def pop(self):
+        if not self.empty():
+            return self.items.pop()
+        else:
+            return -1
+
+    def empty(self):
+        return not self.items
+
+    def top(self):
+        return self.items[-1]
+
+    def output(self):
+        print(self.items)
+
+
+
+T = int(input())
+
+for _ in range(T):
+    S = sys.stdin.readline()
+    stack = Stack()
+
+    flag = True
+    for s in S:
+        if s == '(':
+            stack.push('(')
+        elif s == ')':
+            if stack.pop() == -1:
+                flag = False
+                break
+        else:
+            pass
+
+    if flag and stack.empty():
+        print("YES")
+    else:
+        print("NO")
