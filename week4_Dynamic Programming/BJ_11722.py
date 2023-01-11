86d63b0aed0104d9d556783fb5ea32a0d47f5c88
Chang hun <nick6253@naver.com>
2023-01-11 23:22:09 +0900

    Dynamic Programming / 가장 긴 감소하는 부분 수열

---
+++ week4_Dynamic Programming/BJ_11722.py
@@ -0,0 +1,12 @@
+import sys
+
+N = int(input())
+A = list(map(int, sys.stdin.readline().split()))
+dp = [1] * N
+
+for i in range(1, N):
+    for j in range(i):
+        if A[i] < A[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(max(dp))
