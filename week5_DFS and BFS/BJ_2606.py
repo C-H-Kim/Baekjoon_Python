87830aa31ae29a4c9e2759b203cabf031abffa10
Chang hun <nick6253@naver.com>
2023-02-15 18:00:14 +0900

    DFS and BFS / 바이러스

---
+++ week5_DFS and BFS/BJ_2606.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+
+
+def dfs(start):
+    visited[start] = True
+
+    for i in graph[start]:
+        if not visited[i]:
+            dfs(i)
+
+
+def bfs(start):
+    queue = deque([start])
+    visited[start] = True
+
+    while queue:
+        v = queue.popleft()
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+
+N = int(input())
+M = int(input())
+
+graph = [[] for _ in range(N + 1)]
+
+for _ in range(M):
+    i, j = map(int, sys.stdin.readline().split())
+    graph[i].append(j)
+    graph[j].append(i)
+
+visited = [False] * (N + 1)
+
+bfs(1)
+print(visited.count(True) - 1)
