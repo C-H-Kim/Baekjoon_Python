7db1173763ddc529e0b18579b3e9632adc83095a
Chang hun <nick6253@naver.com>
2023-02-16 23:58:15 +0900

    DFS and BFS / 유기농 배추

--- week5_DFS and BFS/BJ_1012.py
+++ week5_DFS and BFS/BJ_1012.py
@@ -2,7 +2,7 @@ import sys
 from collections import deque
 
 dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 0]
+dy = [0, 0, -1, 1]
 
 
 def bfs(row, col):
@@ -14,25 +14,26 @@ def bfs(row, col):
         for i in range(4):
             nx = r + dx[i]
             ny = c + dy[i]
-            if 0 <= nx < N and 0 <= ny < M:
+            if (0 <= nx < N) and (0 <= ny < M):
                 if graph[nx][ny] == 1:
                     graph[nx][ny] = -1
                     queue.append([nx, ny])
 
 
-M, N, K = map(int, sys.stdin.readline().split())
-graph = [[0] * M for _ in range(N)]
-cnt = 0
+T = int(input())
+for _ in range(T):
+    M, N, K = map(int, sys.stdin.readline().split())
+    graph = [[0] * M for _ in range(N)]
+    cnt = 0
 
-for _ in range(K):
-    y, x = map(int, sys.stdin.readline().split())
-    graph[x][y] = 1
+    for _ in range(K):
+        y, x = map(int, sys.stdin.readline().split())
+        graph[x][y] = 1
 
-for i in range(N):
-    for j in range(M):
-        if graph[i][j] == 1:
-            bfs(i, j)
-            cnt += 1
+    for i in range(N):
+        for j in range(M):
+            if graph[i][j] == 1:
+                bfs(i, j)
+                cnt += 1
 
-print(graph)
-print(cnt)
+    print(cnt)
