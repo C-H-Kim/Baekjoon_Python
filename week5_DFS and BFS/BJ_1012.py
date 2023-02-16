d880652b3eb6ec74491b4f8fe31b97745ea817b7
Chang hun <nick6253@naver.com>
2023-02-16 23:51:24 +0900

    DFS and BFS / 유기농 배추 (Solving)

---
+++ week5_DFS and BFS/BJ_1012.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 0]
+
+
+def bfs(row, col):
+    queue = deque([[row, col]])
+    graph[row][col] = -1
+
+    while queue:
+        r, c = queue.popleft()
+        for i in range(4):
+            nx = r + dx[i]
+            ny = c + dy[i]
+            if 0 <= nx < N and 0 <= ny < M:
+                if graph[nx][ny] == 1:
+                    graph[nx][ny] = -1
+                    queue.append([nx, ny])
+
+
+M, N, K = map(int, sys.stdin.readline().split())
+graph = [[0] * M for _ in range(N)]
+cnt = 0
+
+for _ in range(K):
+    y, x = map(int, sys.stdin.readline().split())
+    graph[x][y] = 1
+
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == 1:
+            bfs(i, j)
+            cnt += 1
+
+print(graph)
+print(cnt)
