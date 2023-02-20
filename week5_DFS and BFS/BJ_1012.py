dcc0fca441b59d56e0fdcab6a453af5a0b1044e0
C-H-Kim <nick6253@naver.com>
2023-02-20 19:55:48 +0900

    DFS and BFS / 유기농 배추

--- week5_DFS and BFS/BJ_1012.py
+++ week5_DFS and BFS/BJ_1012.py
@@ -1,8 +1,19 @@
 import sys
 from collections import deque
+sys.setrecursionlimit(10**6)
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
+
+def dfs(row, col):
+    graph[row][col] = -1
+
+    for i in range(4):
+        nx = row + dx[i]
+        ny = col + dy[i]
+        if (0 <= nx < N) and (0 <= ny < M):
+            if graph[nx][ny] == 1:
+                dfs(nx, ny)
 
 
 def bfs(row, col):
@@ -33,6 +44,7 @@ for _ in range(T):
     for i in range(N):
         for j in range(M):
             if graph[i][j] == 1:
+                #dfs(i, j)
                 bfs(i, j)
                 cnt += 1
 
