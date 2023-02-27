942a13c465c65a1ad26a9d81d7c2ef0190c7646d
Chang hun <nick6253@naver.com>
2023-02-27 13:24:49 +0900

    DFS and BFS / 단지번호붙이기

---
+++ week5_DFS and BFS/BJ_2667.py
@@ -0,0 +1,55 @@
+import sys
+from collections import deque
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+
+def dfs(row, col):
+    global house_cnt
+    graph[row][col] = -1
+
+    for i in range(4):
+        nx = row + dx[i]
+        ny = col + dy[i]
+        if (0 <= nx < N) and (0 <= ny < N):
+            if graph[nx][ny] == 1:
+                dfs(nx, ny)
+                house_cnt += 1
+
+
+def bfs(row, col):
+    global house_cnt
+    queue = deque([[row, col]])
+    graph[row][col] = -1
+
+    while queue:
+        r, c = queue.popleft()
+        for i in range(4):
+            nx = r + dx[i]
+            ny = c + dy[i]
+            if (0 <= nx < N) and (0 <= ny < N):
+                if graph[nx][ny] == 1:
+                    graph[nx][ny] = -1
+                    queue.append([nx, ny])
+                    house_cnt += 1
+
+
+N = int(sys.stdin.readline())
+graph = [list(map(int, sys.stdin.readline().rstrip())) for _ in range(N)]
+house = []
+cnt = 0
+
+for i in range(N):
+    for j in range(N):
+        if graph[i][j] == 1:
+            house_cnt = 1
+            # dfs(i, j)
+            bfs(i, j)
+            house.append(house_cnt)
+            cnt += 1
+
+house.sort()
+print(cnt)
+for elem in house:
+    print(elem)
