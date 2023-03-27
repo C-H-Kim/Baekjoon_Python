32dfd9183f4020f34fc7a4e634bfd6496c81ec23
Chang hun <nick6253@naver.com>
2023-03-27 14:41:23 +0900

    Dynamic Programming / 이항 계수 2

---
+++ week4_Dynamic Programming/BJ_11051.py
@@ -0,0 +1,16 @@
+import sys
+
+N, K = map(int, sys.stdin.readline().split())
+dp = [[0] * (K + 1) for _ in range(N + 1)]
+
+for i in range(N + 1):
+    dp[i][0] = 1
+
+for i in range(K + 1):
+    dp[i][i] = 1
+
+for i in range(1, N + 1):
+    for j in range(1, K + 1):
+        dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % 10007
+
+print(dp[N][K])
