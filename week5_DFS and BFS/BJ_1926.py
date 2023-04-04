58c076a246d6fe3788fb8d91b602e653ab43f473
C-H-Kim <nick6253@naver.com>
2023-04-04 17:35:29 +0900

    DFS and BFS / 그림

--- week5_DFS and BFS/BJ_1926.py
+++ week5_DFS and BFS/BJ_1926.py
@@ -1,15 +1,29 @@
 import sys
 from collections import deque
+sys.setrecursionlimit(10 ** 6)
 
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
 
+def dfs(row, col):
+    global area
+    paper[row][col] = 0
+
+    for i in range(4):
+        nx = row + dx[i]
+        ny = col + dy[i]
+        if (0 <= nx < N) and (0 <= ny < M):
+            if paper[nx][ny] == 1:
+                dfs(nx, ny)
+                area += 1
+
+
 def bfs(row, col):
+    global area
     queue = deque([[row, col]])
     paper[row][col] = 0
-    area = 1
 
     while queue:
         r, c = queue.popleft()
@@ -22,8 +36,6 @@ def bfs(row, col):
                     queue.append([nx, ny])
                     area += 1
 
-    return area
-
 
 N, M = map(int, sys.stdin.readline().split())
 paper = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
@@ -34,7 +46,10 @@ max_area = 0
 for i in range(N):
     for j in range(M):
         if paper[i][j] == 1:
-            max_area = max(max_area, bfs(i, j))
+            area = 1
+            # dfs(i, j)
+            bfs(i, j)
+            max_area = max(max_area, area)
             count += 1
 
 print(count)
