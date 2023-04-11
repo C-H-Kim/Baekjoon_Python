020cf17d625dd9c0a76af3c46f10496bc947731c
Chang hun <nick6253@naver.com>
2023-04-11 21:59:25 +0900

    Binary Search / 수 찾기

---
+++ week7_Binary Search/BJ_1920.py
@@ -0,0 +1,28 @@
+import sys
+
+N = int(sys.stdin.readline())
+A = list(map(int, sys.stdin.readline().split()))
+A.sort()
+
+M = int(sys.stdin.readline())
+num_list = list(map(int, sys.stdin.readline().split()))
+
+ans_list = []
+for num in num_list:
+    start = 0
+    end = N - 1
+    flag = False
+
+    while start <= end:
+        mid = (start + end) // 2
+        if A[mid] > num:
+            end = mid - 1
+        elif A[mid] < num:
+            start = mid + 1
+        else:
+            print(1)
+            flag = True
+            break
+
+    if not flag:
+        print(0)
