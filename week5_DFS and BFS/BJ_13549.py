6fadf8b72b0661eff9ea9a58b17fe9e4740b8c7e
C-H-Kim <nick6253@naver.com>
2023-04-28 14:18:06 +0900

    DFS and BFS / 숨바꼭질 3

--- week5_DFS and BFS/BJ_13549.py
+++ week5_DFS and BFS/BJ_13549.py
@@ -4,28 +4,28 @@ from collections import deque
 
 def bfs(start):
     queue = deque([start])
-    visit[start] = True
+    visited[start] = True
 
     while queue:
         v = queue.popleft()
         if v == K:
             break
 
-        if 0 <= 2 * v <= 100000 and not visit[2 * v]:
+        if 0 <= 2 * v <= 100000 and not visited[2 * v]:
             time[2 * v] = time[v]
-            visit[2 * v] = True
+            visited[2 * v] = True
             queue.append(2 * v)
 
         for i in [v - 1, v + 1]:
-            if 0 <= i <= 100000 and not visit[i]:
+            if 0 <= i <= 100000 and not visited[i]:
                 time[i] = time[v] + 1
-                visit[i] = True
+                visited[i] = True
                 queue.append(i)
 
 
 N, K = map(int, sys.stdin.readline().split())
 time = [0] * 100001
-visit = [False] * 100001
+visited = [False] * 100001
 
 bfs(N)
 
