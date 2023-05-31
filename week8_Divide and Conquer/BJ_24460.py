06774f8d3dbb2e56a599742e348a9eac5d81302c
Chang hun <nick6253@naver.com>
2023-05-31 22:52:20 +0900

    Divide and Conquer / 특별상이라도 받고 싶어

---
+++ week8_Divide and Conquer/BJ_24460.py
@@ -0,0 +1,24 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+
+
+def solution(x, y, n):
+    if n == 1:
+        return matrix[x][y]
+
+    left_top = solution(x, y, n // 2)
+    right_top = solution(x, y + n // 2, n // 2)
+    left_bottom = solution(x + n // 2, y, n // 2)
+    right_bottom = solution(x + n // 2, y + n // 2, n // 2)
+
+    result = [left_top, right_top, left_bottom, right_bottom]
+    result.sort()
+
+    return result[1]
+
+
+N = int(sys.stdin.readline())
+matrix = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+answer = solution(0, 0, N)
+print(answer)
